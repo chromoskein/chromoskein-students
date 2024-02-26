c8912fd9a761c86d3ee2e3b175f60e6286ece5eb
Adam Rychlý <xrychly4@fi.muni.cz>
2024-02-26 16:24:42 +0100

    Adding PCA cluster visualization

--- packages/timeseries/src/utils/interactiveClusters.ts
+++ packages/timeseries/src/utils/interactiveClusters.ts
@@ -2,6 +2,8 @@ import { vec3 } from "gl-matrix";
 import * as Graphics from "lib-graphics";
 import type { Viewport3D } from "lib-graphics";
 import type { ClusterNode } from "./main";
+import { blobFromPoints } from "./main";
+import PCA from 'pca-js';
 
 export class InteractiveClusters {
     root: ClusterLeaf;
@@ -102,7 +104,7 @@ export class ClusterLeaf extends AbstractClusterComposite {
         this.viewport = viewport;
         this.manager = manager;
 
-        this.setVisualisation(HedgehogClusterVisualisation, points);
+        this.setVisualisation(PCAClusterVisualisation, points);
     }
 
     setVisualisation<T extends AbstractClusterVisualisation>(visualisationType: new(manager: InteractiveClusters, points: vec3[], cluster: ClusterNode, viewport: Viewport3D) => T, points: vec3[]) {
@@ -366,6 +368,95 @@ export class HedgehogClusterVisualisation extends AbstractClusterVisualisation {
             viewport.scene.removeObjectByID(this.sphereID);
             this.sphereID = null;
             this.sphere = null;
+        }
+    }
+}
+
+export class PCAClusterVisualisation extends AbstractClusterVisualisation {
+    cluster: ClusterNode;
+    viewport: Viewport3D;
+    coneUp: Graphics.RoundedCone;
+    coneUpID: number | null = null;
+    coneDown: Graphics.RoundedCone;
+    coneDownID: number | null = null;
+
+    constructor(manager: InteractiveClusters, points: vec3[], cluster: ClusterNode, viewport: Viewport3D) {
+        super(manager, points, cluster, viewport);
+
+        this.viewport = viewport;
+
+        [this.coneUp, this.coneUpID] = viewport.scene.addObject(Graphics.RoundedCone);
+        this.coneUp.setDirtyCPU();
+  
+        [this.coneDown, this.coneDownID] = viewport.scene.addObject(Graphics.RoundedCone);
+        this.coneDown.setDirtyCPU();
+
+        this.updateCluster(cluster);
+        this.setColor(cluster.color.rgb);
+    }
+
+    updateCluster(cluster: ClusterNode) {
+        this.cluster = cluster;
+    }
+
+    updatePoints(points: vec3[]) {
+        let blob = blobFromPoints(points.slice(this.cluster.from, this.cluster.to + 1))
+        let result = PCA.getEigenVectors(blob.normalizedPoints);
+       
+        const coneHeight = result[0].eigenvalue / 40.0 + 0.1;
+        const coneOrientation = vec3.fromValues(result[0].vector[0], result[0].vector[1], result[0].vector[2]);
+        const coneStartRadius = result[1].eigenvalue / 40.0 + 0.1;
+
+        this.coneUp.properties.start = [blob.center[0], blob.center[1], blob.center[2]];
+        this.coneUp.properties.end = [
+            blob.center[0] + coneHeight * coneOrientation[0],
+            blob.center[1] + coneHeight * coneOrientation[1],
+            blob.center[2] + coneHeight * coneOrientation[2],
+        ];
+        this.coneUp.properties.startRadius = coneStartRadius;
+        this.coneUp.properties.endRadius = 0.0001;
+        this.coneUp.setDirtyCPU();
+  
+        this.coneDown.properties.start = [blob.center[0], blob.center[1], blob.center[2]];
+        this.coneDown.properties.end = [
+            blob.center[0] - coneHeight * coneOrientation[0],
+            blob.center[1] - coneHeight * coneOrientation[1],
+            blob.center[2] - coneHeight * coneOrientation[2],
+        ];
+        this.coneDown.properties.startRadius = coneStartRadius;
+        this.coneDown.properties.endRadius = 0.0001;
+        this.coneDown.setDirtyCPU();
+    }
+
+    setColor(color: vec3) {
+        if (this.coneDownID && this.coneUpID) {
+            this.coneUp.properties.color = [color[0], color[1], color[2], 1.0];
+            this.coneDown.properties.color = [color[0], color[1], color[2], 1.0];
+            this.coneUp.setDirtyCPU();
+            this.coneDown.setDirtyCPU();
+        }
+    }
+
+    rayIntersection(ray: Graphics.Ray): Graphics.Intersection {
+        let upIntersection = this.coneDown.rayIntersection(ray);
+        let downIntersection = this.coneUp.rayIntersection(ray);
+
+        if (upIntersection == null && downIntersection == null) {
+            return null;
+        }
+        
+        return (downIntersection == null || upIntersection.t < downIntersection.t) ? upIntersection : downIntersection;
+    }
+    
+    delete(viewport: Graphics.Viewport3D) {
+        if (this.coneDownID && this.coneUpID) {
+            viewport.scene.removeObjectByID(this.coneUpID);
+            viewport.scene.removeObjectByID(this.coneDownID);
+            
+            this.coneDownID = null;
+            this.coneDown = null;
+            this.coneUpID = null;
+            this.coneUp = null;
         }
     }
 }
