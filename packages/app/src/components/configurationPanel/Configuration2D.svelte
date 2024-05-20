ff5d0709d90724bcc1173f74632981d072c6b0d4
Adam Rychlý <xrychly4@fi.muni.cz>
2024-05-20 11:00:06 +0200

    Remove app package

--- packages/app/src/components/configurationPanel/Configuration2D.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import type { Writable } from "svelte/store";
-    import type { Configuration2D } from "src/storage/storage";
-
-    export let configuration: Writable<Configuration2D>;
-</script>
-
-<div></div>
-
--- packages/app/src/storage/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./storage";
-export * from "./inBrowserStorage";
-export * from "./fileStorage";
-export * from "./state";
