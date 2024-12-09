2fd2ef9f93ce6baf7c11dcc66a7f341a08cbaadd
Adam Rychlý <adam.rychlybrno@gmail.com>
2024-12-09 14:42:10 +0100

    Fixed bug when clustering timestep data

--- packages/timeseries/src/utils/clusteringWorker.ts
+++ packages/timeseries/src/utils/clusteringWorker.ts
@@ -1,20 +1,19 @@
 import type { vec3 } from "gl-matrix";
-import { clusterPathlines, clusterTimestep, type ClusterNode } from "./main";
+import { clusterPathlines, clusterTimestep, timestepsToPathlines, type ClusterNode } from "./main";
 import { treeColor } from "./treecolors";
 
 self.onmessage = (event: MessageEvent) => {
     const pointTimesteps: vec3[][] = event.data;
-    console.log("Started", event.data);
 
     let clusters: ClusterNode[][] = null;
     // If there is more than a single timestep cluster it as pathlines
     if (pointTimesteps.length > 1) {
-        clusters = clusterPathlines(pointTimesteps).slice(0, 16);
+        const pathlines = timestepsToPathlines(pointTimesteps);
+        clusters = clusterPathlines(pathlines).slice(0, 16);
     } else {
         clusters = clusterTimestep(pointTimesteps[0]).slice(0, 16);
         console.log(clusters);
     }
     treeColor(clusters);
-    console.log("Ended");
     self.postMessage(clusters);
 };
