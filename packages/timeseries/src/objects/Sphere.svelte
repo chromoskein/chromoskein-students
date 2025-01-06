ca0b2e62940b21d4231dd618eecb3ce22cc079c7
Adam Rychlý <adam.rychlybrno@gmail.com>
2025-01-06 16:32:18 +0100

    Update Sphere to runes

--- packages/timeseries/src/objects/Sphere.svelte
+++ packages/timeseries/src/objects/Sphere.svelte
@@ -2,33 +2,34 @@
   import { getContext, onMount } from "svelte";
   import * as Graphics from "@chromoskein/lib-graphics";
   import type { Writable } from "svelte/store";
-  import type { Viewport3D } from "@chromoskein/lib-graphics";
   import { vec3 } from "gl-matrix";
 
-  let viewport: Writable<Graphics.Viewport3D | null> = getContext("viewport");
-
-  export let radius = 0.3;
-  export let center: vec3 = vec3.fromValues(0.0, 0.0, 0.0);
-  export let color: [number, number, number, number] = [1.0, 1.0, 1.0, 1.0];
-
-  let object: Graphics.Sphere | null = null;
-  let objectID: number | null = null;
-
-  $: if ($viewport && $viewport.scene) {
-    if (objectID != null) {
-      $viewport.scene.removeObjectByID(objectID);
-    }
-
-    [object, objectID] = $viewport.scene.addObject(Graphics.Sphere);
-    object.setDirtyCPU();
+  interface SphereProps {
+    radius: number,
+    center: vec3,
+    color: [number, number, number, number]
   }
+   
+  let {
+    radius = 0.3,
+    center = vec3.fromValues(0, 0, 0),
+    color = [1.0, 1.0, 1.0, 1.0]
+  }: SphereProps = $props();
+  
+  let viewport: Writable<Graphics.Viewport3D | null> = getContext("viewport");
+  let [object, objectID]: [Graphics.Sphere | null, number | null] = $derived.by(() => {
+    if ($viewport && $viewport.scene) {
+      return $viewport.scene.addObject(Graphics.Sphere);
+    }
+    return [null, null];
+  });
 
-  $: if (object) {
+  $effect(() => {if (object) {
     object.properties.radius = radius;
     object.properties.center = [center[0], center[1], center[2]];
     object.properties.color = color;
     object.setDirtyCPU();
-  }
+  }});
 
   onMount(() => {
     return () => {
